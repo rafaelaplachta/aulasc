59bb21f6a00baf2f5ad031a175df5645d20a95e7
(Rafaela <(rafaelaplachta@gmail.com)>
2021-06-06 00:51:12 -0300

    Concluindo arquivo fasesDaVida.c com êxito

--- fasesDaVida.c
+++ fasesDaVida.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (void)
+int main(void)
 {
-int anosDeVida;    
+    int anosDeVida;
 
-printf("Qual a sua idade?\n\r");
-scanf("%d", &anosDeVida);
+    printf("Qual a sua idade?\n\r");
+    scanf("%d", &anosDeVida);
 
-if (anosDeVida >= 60)
-{
-    printf("Voce eh uma pessoa idosa!\n\r");
+    if (anosDeVida >= 60)
+    {
+        printf("Voce eh uma pessoa idosa!\n\r");
+    }
+    else if (anosDeVida >= 18)
+    {
+        printf("Voce eh uma pessoa adulta!\n\r ");
+    }
+        else if (anosDeVida >= 11)
+        {
+            printf("Voce eh adolescente\n\r");
 }
-elseif (anosDeVida >=18)
-{
-    printf("Voce eh uma pessoa adulta!\n\r ");
+        else if (anosDeVida >= 3)
+        {
+            printf("Voce eh uma crianca!\n\r");
+    }
+    else
+    {
+        printf("Voce eh um bebe!");
+    }
+    system("pause");
+    return (0);
 }
