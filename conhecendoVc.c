e1abf3f24fc91e5391a21839d7a39dd3463d1ceb
(Rafaela <(rafaelaplachta@gmail.com)>
2021-06-07 17:03:56 -0300

    Descrição do exercício4 no readme e codificando arquivo conhecendoVc.c

--- conhecendoVc.c
+++ conhecendoVc.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <string.h>
 
 int main(void)
 {
     char nome[99];
-printf("Qual seu nome completo?\n\r");
-gets(nome);
+    printf("Qual seu nome completo?\n\r");
+    gets(nome);
 
-int idade;
-printf("Quantos anos voce tem?\n\r");
-scanf("%d", &idade);
+    int idade;
+    printf("Quantos anos voce tem?\n\r");
+    scanf("%d", &idade);
 
-if (idade>=)
+    if (idade >= 65)
+    {
+        printf("Eh uma pessoa idosa\n");
+    }
+    else if (idade >= 18)
+    {
+        printf("Eh uma pessoa adulta\n");
+    }
+    else if (idade >= 11)
+    {
+        printf("Eh adolescente \n");
+    }
+    else
+    {
+        printf("Ainda eh crianca\n");
+    }
 
+    char cidade[99];
+    printf("Em qual cidade voce mora?\n");
+        gets(cidade);
+
+    system("pause");
+    return (0);
+}
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ Esse repositório foi criado para postagens dos exercícios realizados por mim, 
 
 *O arquivo "votaredirigir", chamado de exercício2 na mensagem de commit, foi criado para testar o encadeamento de decisões: if, else if, else.
 *Arquivo "fasesDaVida.c" - exercício3: A partir da idade informada, o programa deve dizer se é uma pessoa idosa, adulta, adolescente, criança ou um bebê.
+*Exercício4" refere-se ao arquivo "conhecendoVc.c"
