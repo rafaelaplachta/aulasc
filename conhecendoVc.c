780ac9afbeb8c9b7d918f1046b168bba8b8767fb
(Rafaela <(rafaelaplachta@gmail.com)>
2021-06-07 16:40:42 -0300

    Criando arquivo para exercício4

---
+++ conhecendoVc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char nome[99];
+printf("Qual seu nome completo?\n\r");
+gets(nome);
+
+int idade;
+printf("Quantos anos voce tem?\n\r");
+scanf("%d", &idade);
+
+if (idade>=)
+
