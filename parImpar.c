a35bd3c539c9a90522c52620ef4df6a74c480190
(Rafaela <(rafaelaplachta@gmail.com)>
2021-06-09 10:37:41 -0300

    Adicionando mais um exercício

---
+++ parImpar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int number, remainder;
+    printf("Ola! Vamos iniciar..\n\r. Comece digitando um numero:\n\r");
+    scanf("%d", &number);
+    remainder= %2;
+
+    if (remainder == 0)
+    {
+        printf("O numero que voce digitou e par");
+    }
+    else
+    {
+        printf("O numero que voce digitou e impar");
+    }
+    return 0;
+    system("pause");
+}
