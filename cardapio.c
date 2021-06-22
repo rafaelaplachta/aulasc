849522a52c02d9fa38262361a896e3b9449ffb54
(Rafaela <(rafaelaplachta@gmail.com)>
2021-06-22 01:36:43 -0300

    Recomeçando o código

--- cardapio.c
+++ cardapio.c
@@ -2,57 +2,71 @@
 #include <stdlib.h>
 #include <conio.h>
 
-void cardapio(void);
-void confirme(void);
-void conta(void);
+const int prato_1 = 161;
+const int prato_2 = 108;
+const int prato_3 = 98;
+const int prato_4 = 94;
+const int prato_5 = 86;
+const int prato_6 = 131;
 
-//início do cardapio.
+void cardapio();
+void outroPrato();
 
-void cardapio(void)
+//cardápio.
+
+void cardapio()
 {
-    int opc;
-    const int prato_1 = 161;
-    const int prato_2 = 108;
-    const int prato_3 = 98;
-    const int prato_4 = 94;
-    const int prato_5 = 93;
-    const int prato_6 = 131;
+    int opcum;
 
-    printf("Ola! Digite o numero referente ao prato que deseja saborear:\n"
-           "1: Camarao Internacional R$ %i reais\n",
-           prato_1
+    printf("Ola! Digite o numero referente ao prato que deseja saborear:\n");
+    printf("1: Camarao Internacional R$ %i reais\n",
+           prato_1);
 
-           "2: Peixe Pizzaiolo R$ %i reais\n",
-           prato_2
+    printf("2: Peixe Pizzaiolo R$ %i reais\n",
+           prato_2);
 
-           "3: Carne de Sol do Sertao R$ %i reais\n",
-           prato_3
-           "4: Camarao Buzios R$ %i reais\n",
-           prato_4
-           "5: Camarao Praia de Olinda R$ %i reais\n",
-           prato_5
-           "6: Tilapia Brasil R$ %i reais\n",
+    printf("3: Carne de Sol do Sertao R$ %i reais\n",
+           prato_3);
+    printf("4: Camarao Buzios R$ %i reais\n",
+           prato_4);
+    printf("5: Camarao Praia de Olinda R$ %i reais\n",
+           prato_5);
+    printf("6: Tilapia Brasil R$ %i reais\n",
            prato_6);
 
-    scanf("%d", &opc);
+    scanf("%i", &opcum);
 
-    if (opc == 1)
+    if (opcum == 1)
     {
         printf("Ok, voce escolheu o prato Camarao Internacional. O valor do prato e R$ %i reais.\n", prato_1);
+        fflush(stdin);
+        return outroPrato();
     }
-    else if (opc == 2)
+    else if (opcum == 2)
     {
         printf("Ok, voce escolheu o prato Peixe Pizzaiolo. O valor do prato e R$ %i reais.\n", prato_2);
+        fflush(stdin);
+        return outroPrato();
     }
     else
     {
         printf("opcao invalida, cardapio em construcao.\n");
+        fflush(stdin);
+        return outroPrato();
     }
+}
 
-    int main(void)
-    {
+//fumcao para escolha de outro prato.
+void outroPrato()
+{
+    printf("Deseja escolher outro prato?\n");
+}
 
-        system("pause");
+int main(void)
+{
+    cardapio();
+    outroPrato();
 
-        return 0;
-    }
+    system("pause");
+    return 0;
+}
