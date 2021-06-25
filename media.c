b7e36b9ed7f9bd27db3be5e6b873e146992c5dba
(Rafaela <(rafaelaplachta@gmail.com)>
2021-06-24 21:47:25 -0300

    Concluindo o programa.

--- media.c
+++ media.c
@@ -9,20 +9,31 @@ int main(void)
     scanf("%f", &nota_1);
 
     printf("Digite a segunda nota e tecle enter:\n");
-    // fflush(stdin);
+    fflush(stdin);
     scanf("%f", &nota_2);
 
     printf("Digite a terceira nota e tecle enter:\n");
-    // fflush(stdin);
+    fflush(stdin);
     scanf("%f", &nota_3);
 
     printf("Digite a quarta nota e tecle enter:\n");
-    // fflush(stdin);
+    fflush(stdin);
     scanf("%f", &nota_4);
 
     media = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
 
-    printf("Sua media final eh %.1f.\n", media);
+    if (media >= 7)
+    {
+        printf("Sua nota final eh %.1f. Voce foi aprovado. Parabens!\n", media);
+    }
+    else if (media < 5)
+    {
+        printf("Sua nota final eh %.1f. Infelizmente voce foi reprovado.\n", media);
+    }
+    else
+    {
+        printf("Sua nota final eh %.1f. Voce vaii precisar fazer prova de exame.\n", media);
+    }
 
     system("pause");
     return 0;
